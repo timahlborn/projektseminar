ee589a5fe24c12972fb2d55df045c4541c73c535
tim.ahlborn <tim.ahlborn@t-online.de>
2018-12-19 16:06:42 +0100

    hallo

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -20,9 +20,6 @@
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
-      <span class="navbar-text">
-      Navbar text with an inline element
-    </span>
     </nav>
   </div>
 </body>
